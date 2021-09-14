6fe4af53a953f6ada0b4535c56905f82d73457b9
Denis Arce <dnsarce21@gmail.com>
2021-09-13 19:40:07 -0500

    updating code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-var davidApiKey = 'cSttLyBiwySAkRm5AYEUUYNrFvs58miI'
-var tmDiv = document.getElementById('ticketMaster');
-var tmPicDiv1 = document.getElementById('picture1');
-var tmNameDiv1 = document.getElementById('name1');
-var tmDateDiv1 = document.getElementById('date1');
-var tmVenueDiv1 = document.getElementById('venue1');
-var tmButton1 = document.getElementById('btn1');
+var davidApiKey = "cSttLyBiwySAkRm5AYEUUYNrFvs58miI";
+var tmDiv = document.getElementById("ticketMaster");
+var tmPicDiv1 = document.getElementById("picture1");
+var tmNameDiv1 = document.getElementById("name1");
+var tmDateDiv1 = document.getElementById("date1");
+var tmVenueDiv1 = document.getElementById("venue1");
+var tmButton1 = document.getElementById("btn1");
 
 // var today = new Date();
 // console.log(today);
@@ -25,122 +25,118 @@ var tmButton1 = document.getElementById('btn1');
 // var fifth = document.getElementById('fifthDay');
 // var sixth = document.getElementById('sixthDay');
 
-
 // //var searchButton = document.getElementsByID('searchBtn');
-    
 
 function getSearchValue(buttonValue) {
+  var inputValue = document.getElementById("searchBand").value;
+  if (buttonValue) {
+    inputValue = buttonValue;
+  }
+  inputValue = inputValue.replace(/\s+/g, "-").toLowerCase();
+  var requestUrl =
+    "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" +
+    inputValue +
+    "&apikey=" +
+    davidApiKey;
+  console.log(requestUrl);
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
 
-    
-    
-    var inputValue = document.getElementById('searchBand').value;
-    if (buttonValue) {
-        inputValue = buttonValue;
-    }
-    inputValue = inputValue.replace(/\s+/g, '-').toLowerCase();
-    var requestUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?keyword=' + inputValue +  '&apikey=' + davidApiKey;
-    console.log(requestUrl);
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-            })
-        .then(function (data) {
-            console.log(data);
+      var artistPic1 = document.createElement("img");
+      var musicPic1 = data._embedded.events[0].images[1].url;
+      artistPic1.src = musicPic1;
+      tmPicDiv1.appendChild(artistPic1);
 
-            
-            var artistPic1 = document.createElement('img');
-            var musicPic1 = data._embedded.events[0].images[1].url;
-            artistPic1.src = musicPic1
-            tmPicDiv1.appendChild(artistPic1);
+      var bandName1 = document.createElement("h3");
+      bandName1.textContent = data._embedded.events[0].name;
+      tmNameDiv1.appendChild(bandName1);
 
-            var bandName1 = document.createElement('h3');
-            bandName1.textContent = data._embedded.events[0].name;
-            tmNameDiv1.appendChild(bandName1);
+      var eventDate1 = document.createElement("h5");
+      eventDate1.textContent = data._embedded.events[0].dates.start.dateTime;
+      tmDateDiv1.appendChild(eventDate1);
 
-            var eventDate1 = document.createElement('h5');
-            eventDate1.textContent = data._embedded.events[0].dates.start.dateTime;
-            tmDateDiv1.appendChild(eventDate1);
+      var concertVenue1 = document.createElement("h5");
+      concertVenue1.textContent =
+        data._embedded.events[0]._embedded.venues[0].name;
+      tmVenueDiv1.appendChild(concertVenue1);
 
-            var concertVenue1 = document.createElement('h5');
-            concertVenue1.textContent = data._embedded.events[0]._embedded.venues[0].name;
-            tmVenueDiv1.appendChild(concertVenue1);
+      var eventURL1 = document.getElementById("anchorTag1");
+      showURL1 = data._embedded.events[0].url;
+      eventURL1.setAttribute("href", showURL1);
 
-            var eventURL1 = document.getElementById('anchorTag1');
-            showURL1 = data._embedded.events[0].url;
-            eventURL1.setAttribute('href', showURL1);
+      // var tmButton1 = document.getElementById('btn1');
+      // var tmButton1.text
 
-            // var tmButton1 = document.getElementById('btn1');
-            // var tmButton1.text
+      //             var name = document.createElement('li');
+      //             name.textContent = data.name;
+      //             city.appendChild(name);
 
+      //             var history = document.createElement('button');
+      //             history.addEventListener('click', function(event) {
+      //                 getSearchValue(event.target.textContent);
+      //             })
+      //             history.className = 'searchBtn'
+      //             history.textContent = data.name;
+      //             historyBtns.appendChild(history);
 
-//             var name = document.createElement('li');
-//             name.textContent = data.name;
-//             city.appendChild(name);
+      //             var newUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=7469b383f606343a74b5e386f6d67047';
+      //             fetch(newUrl)
+      //                 .then(function (response) {
+      //                     return response.json();
+      //                     })
+      //                 .then(function (data) {
+      //                     console.log(data)
 
-//             var history = document.createElement('button');
-//             history.addEventListener('click', function(event) {
-//                 getSearchValue(event.target.textContent);
-//             })
-//             history.className = 'searchBtn'
-//             history.textContent = data.name;
-//             historyBtns.appendChild(history);
+      //                     //todays dashboard
 
-//             var newUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=7469b383f606343a74b5e386f6d67047';
-//             fetch(newUrl)
-//                 .then(function (response) {
-//                     return response.json();
-//                     })
-//                 .then(function (data) {
-//                     console.log(data)
-                    
-//                     //todays dashboard
+      //                     var currentIcon = document.createElement('img');
+      //                     var weatherIcon = data.current.weather[0].icon;
+      //                     currentIcon.src = 'http://openweathermap.org/img/wn/' + weatherIcon + '@2x.png';
+      //                     city.appendChild(currentIcon);
 
-//                     var currentIcon = document.createElement('img');
-//                     var weatherIcon = data.current.weather[0].icon;
-//                     currentIcon.src = 'http://openweathermap.org/img/wn/' + weatherIcon + '@2x.png';
-//                     city.appendChild(currentIcon);
+      //                     var temperature = document.createElement('li');
+      //                     temperature.textContent = 'Temp: ' + data.current.temp + '°F';
+      //                     city.appendChild(temperature);
 
-//                     var temperature = document.createElement('li');
-//                     temperature.textContent = 'Temp: ' + data.current.temp + '°F';
-//                     city.appendChild(temperature);
-                
-//                     var wind = document.createElement('li');
-//                     wind.textContent = 'Wind: ' + data.current.wind_speed + ' MPH';
-//                     city.appendChild(wind);
+      //                     var wind = document.createElement('li');
+      //                     wind.textContent = 'Wind: ' + data.current.wind_speed + ' MPH';
+      //                     city.appendChild(wind);
 
-//                     var humid = document.createElement('li');
-//                     humid.textContent = 'Humidity: ' + data.current.humidity + ' %';
-//                     city.appendChild(humid);
+      //                     var humid = document.createElement('li');
+      //                     humid.textContent = 'Humidity: ' + data.current.humidity + ' %';
+      //                     city.appendChild(humid);
 
-//                     var uvi = document.createElement('li');
-//                     uvi.textContent = 'UV Index: ' + data.current.uvi;
-//                     city.appendChild(uvi);
+      //                     var uvi = document.createElement('li');
+      //                     uvi.textContent = 'UV Index: ' + data.current.uvi;
+      //                     city.appendChild(uvi);
 
-//                     //forecast day 2
+      //                     //forecast day 2
 
-//                     var forecastDate2 = document.createElement('li');
-//                     forecastDate2.textContent = date2
-//                     second.appendChild(forecastDate2);
+      //                     var forecastDate2 = document.createElement('li');
+      //                     forecastDate2.textContent = date2
+      //                     second.appendChild(forecastDate2);
 
-//                     var currentIcon2 = document.createElement('img');
-//                     var weatherIcon2 = data.daily[0].weather[0].icon;
-//                     currentIcon2.src = 'http://openweathermap.org/img/wn/' + weatherIcon2 + '@2x.png';
-//                     second.appendChild(currentIcon2);
+      //                     var currentIcon2 = document.createElement('img');
+      //                     var weatherIcon2 = data.daily[0].weather[0].icon;
+      //                     currentIcon2.src = 'http://openweathermap.org/img/wn/' + weatherIcon2 + '@2x.png';
+      //                     second.appendChild(currentIcon2);
 
-//                     var temperature2 = document.createElement('li');
-//                     temperature2.textContent = 'Temp: ' + data.daily[0].temp.day + '°F';
-//                     second.appendChild(temperature2);
-                
-//                     var wind2 = document.createElement('li');
-//                     wind2.textContent = 'Wind: ' + data.daily[0].wind_speed + ' MPH';
-//                     second.appendChild(wind2);
+      //                     var temperature2 = document.createElement('li');
+      //                     temperature2.textContent = 'Temp: ' + data.daily[0].temp.day + '°F';
+      //                     second.appendChild(temperature2);
 
-//                     var humid2 = document.createElement('li');
-//                     humid2.textContent = 'Humidity: ' + data.daily[0].humidity + ' %';
-//                     second.appendChild(humid2);
+      //                     var wind2 = document.createElement('li');
+      //                     wind2.textContent = 'Wind: ' + data.daily[0].wind_speed + ' MPH';
+      //                     second.appendChild(wind2);
 
+      //                     var humid2 = document.createElement('li');
+      //                     humid2.textContent = 'Humidity: ' + data.daily[0].humidity + ' %';
+      //                     second.appendChild(humid2);
 
-                    
-//                 });
-        });
-}        
+      //                 });
+    });
+}
